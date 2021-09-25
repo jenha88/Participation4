2f3c0a8b34743636bfa94f3fec6724da27877969
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-09-25 12:28:17 -0500

    finish part4

---
+++ Pat4/ContactList/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+        public Contact()
+        {
+            FirstName = String.Empty;
+            LastName = String.Empty;
+            Email = String.Empty;
+        }
+        public Contact(string data)
+        {
+            var file = data.Split('|');
+            FirstName = file[1];
+            LastName = file[2];
+            Email = file[3];
+            Photo = file[4];
+        }
+        public override string ToString()
+        {
+            return $"{LastName}  {FirstName}";
+
+        }
+    }
+}
---
+++ Pat4/ContactList/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Contact> CC = new List<Contact>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var lines = File.ReadAllLines("contacts.txt").Skip(1);
+
+            foreach (var line in lines)
+            {
+                CC.Add(new Contact(line)); //adding items to the list 
+            }
+
+
+
+            foreach (var cc in CC)
+            {
+                lstBox.Items.Add(cc);
+            }
+
+
+        }
+        private void btbDetail_Click(object sender, RoutedEventArgs e)
+        {
+            Contact selectedcontact = (Contact)lstBox.SelectedItem;
+            var uri = new Uri(selectedcontact.Photo);
+            var img = new BitmapImage(uri);
+            imgPhoto.Source = img;
+
+            txtF.Text = selectedcontact.FirstName;
+            txtL.Text = selectedcontact.LastName;
+            txtE.Text = selectedcontact.Email;
+        }
+    }
+    }
+
